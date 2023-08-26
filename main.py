f73b6d67b6a40709002b2181d84c7bf09d66cdc9
ahmadmoneef <110120952+ahmadmoneef@users.noreply.github.com>
2023-08-26 23:09:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+my_name=input('your name?')
+
+my_age=input('your age?')
+
+print(f'My name is {my_name} and I am {my_age} years old')
+
+first_number=int=input('choose the first number')
+
+secound_number=int=input('choose the the secound number')
+
+operation=input('choose a sing to do the operation')
+
+if operation=='+':
+    print('first_number + second_number')
+
+elif operation=='-':
+    print('secound_number-first_number')
+
+elif operation=='*':
+    print(first_number*secound_number)
+
+elif operation=='/':
+    print(first_number/secound_number)
+
+else:
+    print('the operation is not valid')
+
+bus_capacity=48  
+
+people_inbus=(int('how many people is in the bus?'))
+
+waiting=int(input('how many people are waiting?'))
+
+ampty_seatas = bus_capacity - people_inbus
+
+if ampty_seatas>=waiting :
+    print('joinl')
+
+else:
+    print('oops the bus is full')
